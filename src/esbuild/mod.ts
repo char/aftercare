00c097231e91ea5fc85b5b3b2e01fd256f6f2276
Charlotte Som <charlotte@som.codes>
2025-02-04 15:05:05 +0000

    esbuild: enable splitting by default

--- src/esbuild/mod.ts
+++ src/esbuild/mod.ts
@@ -18,6 +18,7 @@ export async function build(opts: BuildOptions) {
   const buildOpts = {
     bundle: true,
     minify: true,
+    splitting: true,
     target: "esnext",
     platform: "browser",
     format: "esm",
