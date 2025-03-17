de867d92a9d7360b4be4527ade1f1c421e450451
Charlotte Som <charlotte@som.codes>
2025-03-17 13:15:11 +0000

    oops

--- src/safely.ts
+++ src/safely.ts
@@ -1,7 +1,7 @@
 // deno-lint-ignore-file no-explicit-any
 export function safely<Args extends any[], R>(
   f: (...args: Args) => R,
-): (...args: Args) => [ok: R | undefined, err: Error | undefined] {
+): (...args: Args) => [ok: R, err: undefined] | [ok: undefined, err: Error] {
   return (...args) => {
     try {
       return [f(...args), undefined] as const;
